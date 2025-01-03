65e83bb69d4c0f5274f6bba06c085e421628fbda
Aditya Jaiswal <adityajaiswal@Adarshs-MacBook-Air.local>
2025-01-03 08:08:35 +0530

    Height and width using useEffect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import ScientistsList from "./Aniket/React-Topics/ScientistsList/ScientistsList";
+import { MyComponent } from "./Aniket/React-Topics/useEffect";
 import { Counter } from "./Aniket/React-Topics/useState/counter";
 function App() {
   // return <Zomato />; ///
   // return <PackingList />;
   // return <ScientistsList />;
-  return <Counter />;
+  // return <Counter />;
+  return <MyComponent />;
 }
 
 export default App;
